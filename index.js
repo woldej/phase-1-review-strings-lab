4587c15b2533a8b3f2540be1d74c5cd31ca89775
woldejamawolde <wolde.jama@m0ringaschool.com>
2023-10-03 23:36:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ';
+const shortGreeting = `Welcome, ${currentUser}`;
+currentUser.slice(0, 1);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
